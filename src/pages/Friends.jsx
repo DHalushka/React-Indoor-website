568728fe86be86fa76f4b0cf46751b250806b97b
DHalushka <galushkodaria@gmail.com>
2023-05-11 15:29:57 +0300

    Finished App

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -1,12 +1,14 @@
+import Chats from "components/Messages/Chats";
+import Search from "components/Messages/Search";
 import React from "react";
 
 function Friends() {
-
 	return (
-		<div>
-			<h1>Welkome to Friends Page</h1>
+		<div className="friends-page">
+			<h1>Your Friends</h1>
+			<Chats />
 		</div>
-	) ;
+	);
 }
 
 export default Friends;
